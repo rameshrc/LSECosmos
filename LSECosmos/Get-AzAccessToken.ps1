8f22c27ae05c1b03cd7bbcc0253fef5f3d733724
Carlo Cardella <carloc@microsoft.com>
2019-05-04 21:43:38 -0700

    add function to retrieve the cached Bearer token from the current powershell context

---
+++ LSECosmos/Get-AzAccessToken.ps1
@@ -0,0 +1,23 @@
+function Get-AzAccessToken {
+    <#
+    .SYNOPSIS
+    Retrieve the cached Azure AccessToken (Bearer) from the current Powershell session and its current Azure Context
+    
+    .NOTES
+    Thanks to Stephane Lapointe (https://www.linkedin.com/in/stephanelapointe/) for this script to get the Bearer Token from an existing Powershell session (https://gallery.technet.microsoft.com/scriptcenter/Easily-obtain-AccessToken-3ba6e593/view/Reviews)
+    #>
+    
+    [CmdletBinding()]
+    param ()
+
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    if (-not $azProfile.Accounts.Count) {
+        Write-Error 'Could not find a valid AzProfile, please run Connect-AzAccount'
+        return
+    }
+
+    $currentAzureContext = Get-AzContext
+    $profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azProfile)
+    $token = $profileClient.AcquireAccessToken($currentAzureContext.Tenant.TenantId)
+    $token.AccessToken
+}
