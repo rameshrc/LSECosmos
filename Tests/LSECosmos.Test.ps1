f8ff2162aa946d739191a350a236e8d60656e2ff
Carlo Cardella <carloc@microsoft.com>
2019-05-05 21:50:17 -0700

    keep working on mock tests

--- Tests/LSECosmos.Test.ps1
+++ Tests/LSECosmos.Test.ps1
@@ -1,21 +1,22 @@
-Import-Module ./LSECosmos/LSECosmos.psm1 -Force -ErrorAction 'Stop'
+Import-Module /Users/carlo/Git/LSECosmos/LSECosmos/LSECosmos.psm1 -Force -ErrorAction 'Stop'
 
 Describe 'Get-AzCosmosDbAccount' {
 
     Mock -CommandName 'Get-AzResource' -MockWith {
-        New-MockObject -Type 'Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResource' @{
+        # New-MockObject -Type 'Selected.Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResource' @{
+        return [pscustomobject]@{
             'Name'              = 'mycosmosaccount';
             'ResourceGroupName' = 'mycosmosaccountRG';
             'ResourceType'      = 'Microsoft.DocumentDb/databaseAccounts';
             'Location'          = 'northcentralus';
             'ResourceId'        = '/subscriptions/f897c2fa-a735-4e03-b019-890cd2f7109e/resourceGroups/mycosmosaccountRG/providers/Microsoft.DocumentDb/databaseAccounts/mycosmosaccount'
         }
-    } -Verifiable
+    }
 
     Context "Without input parameters" {
         It "Should return a PSCustomObject object type" {
-            # Get-AzCosmosDbAccount | Should -BeOfType [pscustomobject]
-            Get-AzCosmosDbAccount | Should -BeOfType 'Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResource'
+            Get-AzCosmosDbAccount | Should -BeOfType [pscustomobject]
+            # Get-AzCosmosDbAccount | Should -BeOfType 'Selected.Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResource'
         }
 
         it "Should return exactly one object" {
@@ -24,7 +25,7 @@ Describe 'Get-AzCosmosDbAccount' {
     }
 
     Context "Filter by AccountName" {
-        It "Given valid -AccountName <Filter> it returns <Expected>" -TestCases @(
+        It "Given valid AccountName <Filter> it returns <Expected>" -TestCases @(
             @{'Filter' = 'mycosmosaccount'; 'Expected' = 'mycosmosaccount' }
             @{'Filter' = 'mycosmosac*'; 'Expected' = 'mycosmosaccount' }
             @{'Filter' = '*cosmosac*'; 'Expected' = 'mycosmosaccount' }
@@ -33,10 +34,10 @@ Describe 'Get-AzCosmosDbAccount' {
             param ($Filter, $Expected)
 
             $account = Get-AzCosmosDbAccount -AccountName $Filter
-            $account.Name | Should -Be 'mycosmosaccount'
+            $account.AccountName | Should -Be 'mycosmosaccount'
         }
 
-        It 'Invalid account name, should return null' {
+        It 'Given invalid AccountName "nonexisting" it returns NULL' {
             $account = Get-AzCosmosDbAccount -AccountName 'nonexisting'
             $account.AccountName | Should -BeNullOrEmpty
         }
