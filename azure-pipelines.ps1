bbd83f38de3a39a67e6d40b566a6d9aef50dbe15
Carlo Cardella <carloc@microsoft.com>
2019-05-10 14:30:34 -0700

    run pester

--- azure-pipelines.ps1
+++ azure-pipelines.ps1
@@ -3,4 +3,6 @@
 install-module az.accounts -Scope CurrentUser -Force
 install-module az.resources -scope currentuser -force
 install-module pester -scope currentuser -force
-gmo -list
+# gmo -list
+
+invoke-pester ./LSECosmos/Tests
