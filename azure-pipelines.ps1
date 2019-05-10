d116562f5b59ad87533b68e60aabfa5218ac9479
Carlo Cardella <carloc@microsoft.com>
2019-05-10 16:24:48 -0700

    improve pester tests, add code coverage

--- azure-pipelines.ps1
+++ azure-pipelines.ps1
@@ -1,9 +1,17 @@
 "current location: $(gl)"
 "script root: $PSScriptRoot"
-install-module az.accounts -Scope CurrentUser -Force
-install-module az.resources -scope currentuser -force
-install-module pester -scope currentuser -force
-# gmo -list
+$modules = gmo -list
 
-# dir -Recurse
-invoke-pester ./Tests/
+if ($modules.name -notcontains 'az.accounts') {
+    Install-Module az.accounts -Scope CurrentUser -Force -SkipPublisherCheck
+}
+if ($modules.Name -notcontains 'az.resources') {
+    Install-Module az.resources -scope currentuser -force -SkipPublisherCheck
+}
+if ($modules.Name -notcontains 'pester') {
+    Install-Module -Name Pester -Force -SkipPublisherCheck
+}
+
+# invoke-pester ./Tests/
+# Invoke-Pester -Script $(System.DefaultWorkingDirectory)\MyFirstModule.test.ps1 -OutputFile $(System.DefaultWorkingDirectory)\Test-Pester.XML -OutputFormat NUnitXML
+Invoke-Pester -Script ./Tests/ -OutputFile "./Test-Pester.XML" -OutputFormat 'NUnitXML' -CodeCoverage "./LSECosmos/*.ps1"
