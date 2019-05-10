6fb400cd8a9f1345ba47e4770c5a0e1d51e93073
Carlo Cardella <carloc@microsoft.com>
2019-05-10 14:26:46 -0700

    install modules

--- azure-pipelines.ps1
+++ azure-pipelines.ps1
@@ -1,2 +1,6 @@
 "current location: $(gl)"
 "script root: $PSScriptRoot"
+install-module az.accounts -Scope CurrentUser -Force
+install-module az.resources -scope currentuser -force
+install-module pester -scope currentuser -force
+gmo -list
