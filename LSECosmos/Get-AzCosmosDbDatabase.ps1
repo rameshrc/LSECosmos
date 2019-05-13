85a013441f6bbf099f557caffebb62aed681b54f
Carlo Cardella <carloc@microsoft.com>
2019-05-12 17:26:26 -0700

    get cosmos databases for a given account

--- LSECosmos/Get-AzCosmosDbDatabase.ps1
+++ LSECosmos/Get-AzCosmosDbDatabase.ps1
@@ -9,11 +9,19 @@ function Get-AzCosmosDbDatabase {
         [string]$ResourceGroupName,
 
         [parameter()]
-        [Alias('DatabaseName')]
+        [Alias('DatabaseName', 'Id')]
         [string]$Name = '*'
     )
 
     process {
-        Invoke-AzResourceAction -ResourceName $AccountName -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.DocumentDB/databaseAccounts/apis/databases' -Action 'read' -ApiVersion '2016-03-31' -Force
+        # Invoke-AzResourceAction -ResourceName $AccountName -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.DocumentDB/databaseAccounts/apis/databases' -Action 'read' -ApiVersion '2016-03-31' -Force
+        $outObj = Get-AzResource -ResourceType "Microsoft.DocumentDb/databaseAccounts/apis/databases" -ApiVersion "2015-04-08" -ResourceGroupName $ResourceGroupName -Name "$AccountName/sql/" #| Select-Object -ExpandProperty 'Properties'
+
+        if ($Name -eq '*') {
+            $outObj.Properties
+        }
+        else {
+            $outObj.Properties | Where-Object 'Id' -EQ $Name
+        }
     }
 }
