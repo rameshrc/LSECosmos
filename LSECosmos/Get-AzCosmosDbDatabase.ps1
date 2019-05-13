2ad59c5dbce6047afbbc2ee51c513536583b58ab
Carlo Cardella <carloc@microsoft.com>
2019-05-12 22:44:51 -0700

    remove comment

--- LSECosmos/Get-AzCosmosDbDatabase.ps1
+++ LSECosmos/Get-AzCosmosDbDatabase.ps1
@@ -14,7 +14,6 @@ function Get-AzCosmosDbDatabase {
     )
 
     process {
-        # Invoke-AzResourceAction -ResourceName $AccountName -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.DocumentDB/databaseAccounts/apis/databases' -Action 'read' -ApiVersion '2016-03-31' -Force
         $outObj = Get-AzResource -ResourceType "Microsoft.DocumentDb/databaseAccounts/apis/databases" -ApiVersion "2015-04-08" -ResourceGroupName $ResourceGroupName -Name "$AccountName/sql/" #| Select-Object -ExpandProperty 'Properties'
 
         if ($Name -eq '*') {
