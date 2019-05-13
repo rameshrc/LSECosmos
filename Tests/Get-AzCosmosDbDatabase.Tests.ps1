7a289885ba8b83a7d830e6204c1fa2d9a66b648f
Carlo Cardella <carloc@microsoft.com>
2019-05-12 20:39:05 -0700

    add Get-AzCosmosDbDatabase tests

---
+++ Tests/Get-AzCosmosDbDatabase.Tests.ps1
@@ -0,0 +1,40 @@
+Remove-Module 'LSECosmos' -Force -ErrorAction 'SilentlyContinue'
+Import-Module $PSScriptRoot/../LSECosmos/LSECosmos.psm1 -Force -ErrorAction 'Stop'
+
+Describe 'Get-AzCosmosDbDatabase' {
+    InModuleScope 'LSECosmos' {
+        Mock 'Get-AzResource' -MockWith {
+            # make sure to return the proper object type
+            $generic = [Microsoft.Azure.Management.ResourceManager.Models.GenericResource]::new()
+            $resource = [Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResource]::new($generic)
+            $resource.Name = 'carloccosmos'
+            $resource.ResourceGroupName = 'cosmosrg'
+            $resource.Properties = [pscustomobject]@{
+                'Id'     = 'Database1';
+                '_rid'   = 'xxxxxxxx';
+                '_self'  = 'xxxxxxxxxxxxx';
+                '_etag'  = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+                '_colls' = 'colls/';
+                '_users' = 'users/';
+                '_ts'    = 'xxxxxxxxxx'
+            }
+            $resource
+        }
+
+        Context 'Without input parameters should return one object' {
+            it "Should return exactly one object" {
+                LSECosmos\Get-AzCosmosDbDatabase -AccountName 'carloccosmos' -ResourceGroupName 'cosmosrg' | Should -HaveCount 1
+            }
+
+            it 'Searchig for a non existing database, it should not return anything' {
+                LSECosmos\Get-AzCosmosDbDatabase -AccountName 'carloccosmos' -ResourceGroupName 'cosmosrg' -Name 'nonexistingdatabase' | Should -BeNullOrEmpty
+            }
+
+            It 'Passing a non existing AccountName it should return an error' {
+                { LSECosmos\Get-AzCosmosDbDatabase -AccountName 'nonexistingaccount' -ResourceGroupName 'cosmosrg' -ErrorVariable 'err' }
+                $err.Count | Should -Be 1
+                $err | Should -Be ("ResourceGroupNotFound : Resource group 'cosmosrg' could not be found.")
+            }
+        }
+    }
+}
